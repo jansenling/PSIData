eec01737714ab36ae6a37882f22abcb30ab540c4
jansenling <jansen_ling@nea.gov.sg>
2014-09-21 17:35:31 +0800

    Include help text
    Insert simple help text at the left panel.

--- ui.R
+++ ui.R
@@ -7,8 +7,20 @@ shinyUI(fluidPage(
   ## Sidebar with selecting input file in csv format
   sidebarLayout(
   sidebarPanel(
-    fileInput('file1', 'Select 24-HR PSI.CSV File')
+    fileInput('file1', 'Import 24-HR PSI dataset'),
     
+    helpText("This is a simple Shiny app for reading a dataset",
+             "(csv format) of Pollution Standard Index (PSI) ",
+             "across 5 regions (N, S, E, W and Central) over ",
+             "a 24-hour period. The application will first ",
+             "import the sample csv (24-HR PSI.csv). "),
+    helpText( "                                      "),
+    helpText( " [Table] tab -- display a table of the dataset. "),
+    helpText( "                                      "),
+    helpText( " [Plot] tab -- plots of the PSI values vs freq. "),
+    helpText( "                                       "),
+    helpText( " [Summary] tab -- provide details such as",
+             " means, meadian, min, max and quartile values.")
   ),
   
   # Show tabsets that includes a plot and table view
